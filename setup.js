f12db5c03d248a3a1ee8df635193323fd86f9165
Alejandro Cremades <ale@alecrem.com>
2017-11-07 14:29:15 +0900

    Creating the HTML pages to show the data we will back up from Ethermine

---
+++ setup.js
@@ -0,0 +1,46 @@
+/*
+ * Writes the HTML files based on the accounts and timespans on settings.json
+ */
+
+var fs = require('fs');
+var script_directory = process.argv[1].substring(0, process.argv[1].lastIndexOf("/") + 1) + '/';
+var config = JSON.parse(fs.readFileSync(script_directory + '/settings.json', 'utf8'));
+var html_files = [];
+var html_template = null;
+
+config.accounts.forEach(account => {
+  config.timespans.forEach(timespan => {
+    html_files.push({
+      'filename': 'stats_' + account.name + timespan.days + '.html',
+      'accountname': account.name,
+      'address': account.address,
+      'timespan': timespan.days,
+    });
+  });
+});
+
+html_files.forEach(html_file => {
+  fs.access(script_directory + html_file.filename, fs.constants.F_OK, (err) => {
+    console.log(err ? '😲 ' + html_file.filename + ' does not exist' : '👍 ' + html_file.filename + ' exists');
+    if (err) {
+      if(html_template === null){
+        html_template = fs.readFileSync(script_directory + '/template.html', 'utf8');
+        html_template = html_template.split("\n");
+      }
+      var file_lines = [];
+      html_template.forEach(line => {
+        file_lines.push(
+          line
+          .replace("%%ACCOUNTNAME%%", html_file.accountname)
+          .replace("%%ADDRESS%%", html_file.address)
+          .replace("%%TIMESPAN%%", html_file.timespan)
+        );
+      });
+      var file_content = file_lines.join("\n");
+      fs.writeFile(script_directory + html_file.filename, file_content, 'utf8', (err) => {
+        if (err) throw err;
+        else console.log('✍️ ' + html_file.filename + ' has been saved!');
+      });
+    }
+  });
+});
